8953d649d70d1a78e5cbeebf9c1187e033d9cb6f
Ariel Bendahan <ariellielrey@hotmail.com>
2024-11-12 09:39:18 -0500

    Added admin user type so now admins could login and go to admin page. Admin Page needs functionality added later

--- app/src/main/java/com/example/simubetproject/LoginActivity.java
+++ app/src/main/java/com/example/simubetproject/LoginActivity.java
@@ -1,3 +1,5 @@
+// This activity is responsible for the login functionality of the app. It allows users to log in with their email and password.
+// It also checks if the user is an admin or a regular user and redirects them to the appropriate page.
 package com.example.simubetproject;
 
 import android.content.Intent;
@@ -5,26 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText emailText, passwordText;
     Button loginButton, registerLinkButton;
     FirebaseAuth mAuth;
+    DatabaseReference databaseUsers;
+    ProgressBar loadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +43,9 @@ public class LoginActivity extends AppCompatActivity {
         passwordText = findViewById(R.id.passwordLoginText);
         loginButton = findViewById(R.id.loginButton);
         registerLinkButton = findViewById(R.id.signupLinkButton);
+        loadingIndicator = findViewById(R.id.loadingIndicator);
         mAuth = FirebaseAuth.getInstance();
+        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
 
         registerLinkButton.setOnClickListener(view -> {
             Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
@@ -61,30 +70,83 @@ public class LoginActivity extends AppCompatActivity {
             return;
         }
 
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>()
-        {
+        // Sign in the user with the provided email and password and check if the user is an admin or a regular user
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
-                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    if (user != null) {
+                        loadingIndicator.setVisibility(View.VISIBLE);
+                        String userId = user.getUid();
+                        databaseUsers.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                loadingIndicator.setVisibility(View.GONE);
+                                User user = snapshot.getValue(User.class);
+                                if (user != null) {
+                                    String userRole = user.getType();
+                                    if ("admin".equals(userRole)) {
+                                        Intent intent = new Intent(LoginActivity.this, AdminPage.class);
+                                        startActivity(intent);
+                                    } else {
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                    }
+                                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+                                Toast.makeText(LoginActivity.this, "Failed to load user data", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
                 } else {
                     Toast.makeText(LoginActivity.this, "Login failed. Please make sure your credentials are correct!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
-
+// If the user is already logged in, the app will redirect them to the appropriate page based on their role
     @Override
     protected void onStart() {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            loadingIndicator.setVisibility(View.VISIBLE);
+            String userId = currentUser.getUid();
+            databaseUsers.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    loadingIndicator.setVisibility(View.GONE);
+                    // Get the user object from the database based on the user ID
+                    User user = snapshot.getValue(User.class);
+                    // Check if the user exists
+                    if (user != null) {
+                        // Get the user role and redirect to the appropriate page
+                        String userRole = user.getType();
+                        if ("admin".equals(userRole)) {
+                            Intent intent = new Intent(LoginActivity.this, AdminPage.class);
+                            startActivity(intent);
+                        } else {
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        Toast.makeText(LoginActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+
+                // Error handling in case the user data cannot be retrieved
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    loadingIndicator.setVisibility(View.GONE);
+                    Toast.makeText(LoginActivity.this, "Failed to load user data", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
